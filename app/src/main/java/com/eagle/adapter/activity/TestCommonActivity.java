ccce0bdd84528ca0336ef1a1e1356273f1c58ae9
Rocky <1247106107@qq.com>
2017-11-20 20:06:06 +0800

    add image utils

--- app/src/main/java/com/eagle/adapter/activity/TestCommonActivity.java
+++ app/src/main/java/com/eagle/adapter/activity/TestCommonActivity.java
@@ -3,13 +3,18 @@ package com.eagle.adapter.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.eagle.adapter.R;
+import com.eagle.adapter.util.ImageUtils;
+
 
 public class TestCommonActivity extends BaseActivity {
 
     private TextView txtv_JustTest;
+    private ImageView imgv_TestSketch;
 
     @Override
     public int getContentLayout() {
@@ -21,12 +26,15 @@ public class TestCommonActivity extends BaseActivity {
         setToolBarTitle("Common Test");
         txtv_JustTest = findViewById(R.id.txtv_JustTest);
         txtv_JustTest.setOnClickListener(view -> {
-            finish();
+            imgv_TestSketch.setImageBitmap(ImageUtils.changeToSketch(BitmapFactory.decodeResource(getResources(), R.mipmap.just_test_one)));
         });
+        imgv_TestSketch = findViewById(R.id.imgv_TestSketch);
     }
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, TestCommonActivity.class);
         context.startActivity(intent);
     }
+
+
 }
